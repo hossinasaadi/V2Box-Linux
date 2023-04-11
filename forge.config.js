988fefa6f26fb1b1f2d5d2940f6bb9a8ce8b04ac
Hossin Asaadi <hossin.asaadi@gmail.com>
2023-04-11 10:35:23 -0400

    fix forge

--- forge.config.js
+++ forge.config.js
@@ -3,8 +3,27 @@ module.exports = {
   rebuildConfig: {},
   makers: [
     {
+      name: "@electron-forge/maker-zip",
+      platforms: [
+        "darwin"
+      ]
+    },
+
+    {
       name: '@electron-forge/maker-deb',
       config: {},
     }
   ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'hossinasaadi',
+          name: 'V2Box-Linux'
+        },
+        prerelease: true
+      }
+    }
+  ]
 };
